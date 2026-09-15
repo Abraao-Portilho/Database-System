bc85c0eded281433363f0cc4e748c39fcb1a6732
João Paulo Silva <joaopaulomfs@gmail.com>
2026-09-14 22:06:31 -0300

    implementa páginas, registros e cabeçalho com tamanho fixo

--- service.py
+++ service.py
@@ -1,20 +1,33 @@
-def gravador(local_arquivo, page_id, dados):
+import sys
+import os
+def gravador(local_arquivo, page_id,slot_id, dados:bytes):
     tamanho_pag = 4096
-    with open(local_arquivo , "w+b") as arquivo:
-        inicio_gravacao = tamanho_pag * page_id
-        arquivo.seek(inicio_gravacao)
-        bytes = dados.encode("utf-8")
-        arquivo.write(bytes)
+    tamanho_cabecalho = 16
+    tamanho_registro = 8
+    if not os.path.exists(local_arquivo):
+        with open(local_arquivo , "wb") as arquivo:
+            inicio_gravacao =tamanho_pag * page_id
+            arquivo.seek(inicio_gravacao)
+            arquivo.write(bytearray(tamanho_pag)) #preenchendo a página de bytes de valores nulo
+    if len(dados) <= tamanho_registro:
+        with open(local_arquivo, "r+b") as arquivo: #gravando os dados
+            inicio_gravacao = (tamanho_pag *page_id) +tamanho_cabecalho + (tamanho_registro*slot_id)
+            arquivo.seek(inicio_gravacao)
+            arquivo.write(dados)
+    else: 
+        print("Tamanho dos dados excede o tamanho máximo de um registro")
+
+
                    
 def leitor(local_arquivo , page_id):
     tamanho_pag = 4096
     with open(local_arquivo , "rb") as arquivo:
         inicio_leitura = tamanho_pag * page_id
         arquivo.seek(inicio_leitura) #seek serve para indicar onde será o inicio da leitura
+        dados_pagina= arquivo.read(tamanho_pag)
 
-        dados_pagina = arquivo.read(tamanho_pag)
-        dados_em_int = int.from_bytes(dados_pagina)
-        return dados_em_int
+        return dados_pagina
+        
 
 
 
